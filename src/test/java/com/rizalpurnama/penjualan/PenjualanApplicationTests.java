6c7830a21b0defd502f2764846f7eb4b932e7f3e
rizal <rizal_purnama22@yahoo.com>
2021-10-10 02:44:33 +0700

    first commit

---
+++ src/test/java/com/rizalpurnama/penjualan/PenjualanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizalpurnama.penjualan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenjualanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
